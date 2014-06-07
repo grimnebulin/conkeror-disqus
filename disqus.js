49bd9a86fc309f4ba8a0c8b3730a129d20347687
Sean McAfee <eefacm@gmail.com>
2014-06-07 11:47:44 -0700

    Refactoring; adds various keyboard commands.

--- disqus.js
+++ disqus.js
@@ -1,37 +1,73 @@
-$$.static.nuke_disqus_indicators = function () {
-    [ "north", "south" ].forEach(function (dir) {
-        this.repeat(9e9, 2000, function () {
-            return this("iframe#dsq-indicator-" + dir).remove();
-        });
-    }, this);
+$$.static.disqusIframe = function () {
+    return this("iframe[src*='disqus.com/embed/comments/']").eq(0);
+}
+
+$$.static.disqus = function () {
+    if (/\bdisqus\.com\/embed\/comments\//.test(this.window.location)) {
+        return Some(this);
+    } else {
+        return this.disqusIframe().enterIframe();
+    }
 };
 
-$$.static.load_all_disqus_comments = function () {
-    this.repeat(1000, 500, function () {
-        let button = this("div#disqus_thread > iframe[src*='/embed/comments/']")
-            .inIframe("div.load-more > a");
-        if (!button.is(":visible")) return true;
-        button.not(".busy").clickthis();
+function pop_out_disqus_comments(I) {
+    $$(I).disqusIframe()
+         .each(function () {
+             browser_object_follow(I.buffer, OPEN_NEW_BUFFER, this.src);
+         })
+         .length > 0 || I.minibuffer.message(
+             "No Disqus iframes on this page!"
+         );
+}
+
+define_key(default_global_keymap, "C-c d p", pop_out_disqus_comments);
+
+function load_more_disqus_comments(I) {
+    $$(I).disqus()
+         .foreach($ => $("div.load-more > a.btn").not(".busy").clickthis())
+         .nonempty || I.minibuffer.message(
+             "No Disqus comments present, as far as I can tell."
+         );
+}
+
+define_key(default_global_keymap, "C-c d m", load_more_disqus_comments);
+
+function load_all_disqus_comments(I) {
+    $$(I).disqus()
+         .foreach(function ($) {
+             let hasBeenVisible = false;
+             $.repeat(1000, 500, function () {
+                 const button = this("div.load-more > a.btn");
+                 if (button.hasClass("visible")) {
+                     hasBeenVisible = true;
+                 } else if (hasBeenVisible) {
+                     return true;
+                 }
+                 if (!button.hasClass("busy"))
+                     button.clickthis();
+                 return false;
+             });
+         })
+        .nonempty || I.minibuffer.message(
+            "No Disqus comments present, as far as I can tell."
+        );
+}
+
+define_key(default_global_keymap, "C-c d a", load_all_disqus_comments);
+
+
+function make_disqus_bearable($) {
+    $("div#discovery-top").remove();
+    $.repeat(9e9, 5000, function () {
+        this("iframe#dsq-indicator-north").remove();
+        this"iframe#dsq-indicator-south").remove();
+        this("a.see-more").clickthis();
         return false;
     });
-};
+}
 
-$$.static.load_all_disqus_comments2 = function () {
-    let hasBeenVisible = false;
-    this.repeat(1000, 500, function () {
-        const button = this("div.load-more > a.btn")
-        if (button.is(":visible")) {
-            hasBeenVisible = true;
-        } else if (hasBeenVisible) {
-            return true;
-        }
-        if (!button.hasClass("busy"))
-            button.clickthis();
-        return false;
+add_dom_content_loaded_hook(function (buffer) {
+    $$(buffer).repeat(120, 500, function () {
+        return this.disqus().foreach(make_disqus_bearable).nonempty;
     });
-};
-
-$$.static.make_disqus_bearable = function () {
-    this.nuke_disqus_indicators();
-    this.load_all_disqus_comments();
-};
+});
